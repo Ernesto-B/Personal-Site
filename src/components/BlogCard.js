0e4b6066123f90808c4a2dc9fd20f25f013a57bf
Ernesto-B <ernesto.barreto@ucalgary.ca>
2024-11-10 18:27:27 -0700

    Blog page images are dimmed

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -6,7 +6,10 @@ function BlogCard({ post }) {
     return (
         <Link to={`/blog/${post.id}`} className="blog-card-link">
             <div className="blog-card">
-                <img src={post.image} alt={post.title} className="blog-image" />
+                <div className="blog-image-container">
+                    <img src={post.image} alt={post.title} className="blog-image" />
+                    <div className="blog-image-overlay"></div> {/* Overlay only affects the image */}
+                </div>
                 <div className="blog-content">
                     <div className="blog-left">
                         <h3>{post.title}</h3>
